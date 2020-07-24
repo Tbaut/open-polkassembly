65d9d4f1a9671d32985d01ac18e1671af2ea4437
thib <thibaut.sardan@gmail.com>
2020-07-24 15:14:48 +0200

    comment check done

--- front-end/src/components/Post/TextileStatus.tsx
+++ front-end/src/components/Post/TextileStatus.tsx
@@ -7,14 +7,14 @@ import styled from '@xstyled/styled-components';
 import React, { useEffect, useState } from 'react';
 import { Popup } from 'semantic-ui-react';
 import { CommentFieldsFragment } from 'src/generated/graphql';
-import { useTextileFindPosts } from 'src/hooks';
+import { useTextileFindComments, useTextileFindPosts } from 'src/hooks';
 
 interface Props{
 	author?: string | null;
     className?: string;
+	comments? : CommentFieldsFragment[];
     content?: string | null;
     id: number;
-	comments? : CommentFieldsFragment[];
 	title? : string | null;
 }
 
@@ -41,51 +41,94 @@ const DivContent = styled.div`
 	color: black_text;
 `;
 
-const TextileStatus = ({ author, className, content, id, title } : Props) => {
-	const [isValid, setIsValid] = useState<boolean | null>(null);
-	const [findPost, { data, error }] = useTextileFindPosts();
+const TextileStatus = ({ author, className, comments, content, id, title } : Props) => {
+	const [isValidPost, setIsValidPost] = useState<boolean | null>(null);
+	const [findPosts, { data: dataPosts, error: errorPosts }] = useTextileFindPosts();
+	const [isValiComments, setIsValidComments] = useState<boolean | null>(null);
+	const [findComments, { data: dataComments, error: errorComments }] = useTextileFindComments();
 	const [popupText, setPopupText] = useState('Verifying content on IPFS...');
 
 	useEffect(() => {
-		findPost(new Where('_id').eq(id.toString()));
-	}, [findPost, id]);
+		findPosts(new Where('_id').eq(id.toString()));
+	}, [findPosts, id]);
 
 	useEffect(() => {
-		if(error){
-			console.error('error', error);
+		findComments(new Where('postId').eq(id.toString()));
+	}, [findComments, id, comments]);
+
+	useEffect(() => {
+		if(errorPosts){
+			console.error('errorPosts', errorPosts);
 		}
-	}, [error]);
+
+		if(errorComments){
+			console.error('errorComments', errorComments);
+		}
+	}, [errorPosts, errorComments]);
 
 	useEffect(() => {
-		console.log('data',data);
+		console.log('dataPosts',dataPosts);
 
-		if (data?.instancesList.length){
-			const { content: textileContent, title: textileTitle, author: textileAuthor } = data.instancesList[0];
+		if (dataPosts?.instancesList.length){
+			const { content: textileContent, title: textileTitle, author: textileAuthor } = dataPosts.instancesList[0];
 
 			if (content !== textileContent){
 				setPopupText('Post content donot match between this page and what is stored on IPFS. This page should not be trusted.');
-				console.error('Content donnot match!', content, textileContent);
-				setIsValid(false);
+				console.error(`Content donnot match! ${content} !== ${textileContent}`);
+				setIsValidPost(false);
 			} else if (title !== textileTitle){
 				setPopupText('Post title donot match between this page and what is stored on IPFS. This page should not be trusted.');
-				console.error('Title donot match!', title, textileTitle);
-				setIsValid(false);
+				console.error(`Title donot match! ${title} !== ${textileTitle}`);
+				setIsValidPost(false);
 			} else if (author !== textileAuthor){
 				setPopupText('Post author donot match between this page and what is stored on IPFS. This page should not be trusted.');
-				console.error('Author donot match!', author, textileAuthor);
-				setIsValid(false);
+				console.error(`Author donot match! ${author} !== ${textileAuthor}`);
+				setIsValidPost(false);
 			} else {
 				setPopupText('The content on this page matches with the one stored on IPFS!');
-				setIsValid(true);
+				setIsValidPost(true);
 			}
 		}
 
-		if (data?.instancesList.length === 0){
+		if (dataPosts?.instancesList.length === 0){
 			setPopupText('No data corresponding to this id could be found on IPFS. This page should not be trusted.');
-			setIsValid(false);
+			setIsValidPost(true);
 		}
 
-	}, [author, data, content, title]);
+	}, [author, dataPosts, content, title]);
+
+	useEffect(() => {
+		console.log('dataComments',dataComments);
+
+		if (dataComments?.instancesList.length && comments?.length){
+			comments.forEach(({ author, content }, index) => {
+				const { author: IPFSAuthor, content: IPFSContent } = dataComments?.instancesList[index] || {};
+				const username = author?.username;
+
+				console.log('username !== IPFSAuthor',username, IPFSAuthor);
+				console.log('content !== IPFSContent', content, IPFSContent);
+
+				if(username !== IPFSAuthor){
+					setPopupText('Author in comments donot match between this page and what is stored on IPFS. This page should not be trusted.');
+					console.error(`Comment author donnot match! ${IPFSAuthor} !== ${username}`);
+					setIsValidComments(false);
+				} else if(content !== IPFSContent){
+					setPopupText('Content in comments donot match between this page and what is stored on IPFS. This page should not be trusted.');
+					console.error(`Comment content donnot match! ${content} !== ${IPFSContent}`);
+					setIsValidComments(false);
+				} else {
+					setPopupText('The content on this page matches with the one stored on IPFS!');
+					setIsValidComments(true);
+				}
+			});
+		}
+
+		if (dataComments?.instancesList.length === 0){
+			setPopupText('No data corresponding to this id could be found on IPFS. This page should not be trusted.');
+			setIsValidPost(true);
+		}
+
+	}, [dataComments, comments]);
 
 	return (
 		<div className={className}>
@@ -93,9 +136,9 @@ const TextileStatus = ({ author, className, content, id, title } : Props) => {
 				className={className}
 				trigger={<SpanContent
 					className={
-						isValid === null
+						(isValidPost === null || isValiComments === null)
 							? 'grey'
-							: isValid === true
+							: (isValidPost === true && isValiComments === true)
 								? 'green'
 								: 'red'
 					}>●</SpanContent>}
